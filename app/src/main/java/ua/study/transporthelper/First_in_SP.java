94798ad4c2a85bef3a4ba0d4e0a5edbc2d050536
Artem Nishchenko <artem.nishchenko0@gmail.com>
2020-03-20 13:26:26 +0200

    First step for checking new user.

---
+++ app/src/main/java/ua/study/transporthelper/First_in_SP.java
@@ -0,0 +1,32 @@
+package ua.study.transporthelper;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class First_in_SP extends Activity {
+    public static final String MY_SETTINGS = "settings";
+    public static final String FIRST_VISIT_KEY = "hasVisited";
+    private boolean has_visited;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sp = getSharedPreferences(MY_SETTINGS, Context.MODE_PRIVATE);
+        //First time?
+        has_visited = sp.getBoolean(FIRST_VISIT_KEY, false);
+
+        if(!has_visited)
+        {
+            //TODO Вывод нужной активити
+            SharedPreferences.Editor e = sp.edit();
+            e.putBoolean(FIRST_VISIT_KEY, true);
+            e.commit();
+        }
+    }
+}
