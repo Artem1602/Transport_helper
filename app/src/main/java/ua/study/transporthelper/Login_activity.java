a69968eef738f37e7d8d34b357e66ad77763affc
Oleksiy-me <maksymenko1111@gmail.com>
2020-03-20 18:14:16 +0300

    login activity

--- app/src/main/java/ua/study/transporthelper/Login_activity.java
+++ app/src/main/java/ua/study/transporthelper/Login_activity.java
@@ -10,5 +10,6 @@ public class Login_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
     }
 }
