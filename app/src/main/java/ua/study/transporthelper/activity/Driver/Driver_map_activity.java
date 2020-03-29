f030be2cca6327deac24151d2be845e7f3250a4d
Artem Nishchenko <artem.nishchenko0@gmail.com>
2020-03-29 16:52:40 +0300

    Start to adding info for driver

--- app/src/main/java/ua/study/transporthelper/activity/Driver/Driver_map_activity.java
+++ app/src/main/java/ua/study/transporthelper/activity/Driver/Driver_map_activity.java
@@ -9,8 +9,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
@@ -33,16 +33,20 @@ import com.google.firebase.database.ValueEventListener;
 import ua.study.transporthelper.R;
 import ua.study.transporthelper.settings.User_Firebase;
 
-public class Driver_map_activity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnInfoWindowClickListener {
+public class Driver_map_activity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnInfoWindowClickListener,
+View.OnClickListener{
 
     private GoogleMap mMap;
     private FirebaseDatabase database;
     private DatabaseReference reference;
+    private Button info_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.driver_map_layout);
+        info_btn = findViewById(R.id.info_btn);
+        info_btn.setOnClickListener(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -78,14 +82,25 @@ public class Driver_map_activity extends FragmentActivity implements OnMapReadyC
 
             @Override
             public View getInfoContents(Marker marker) {
-                View v = getLayoutInflater().inflate(R.layout.info_show,null);
-                TextView name = v.findViewById(R.id.name_info);
-                TextView number = v.findViewById(R.id.number_info);
-                TextView info = v.findViewById(R.id.info_info);
-                name.setText(marker.getTitle());
-                number.setText(marker.getSnippet().split("/")[0]);
-                info.setText(marker.getSnippet().split("/")[1]);
-                return v;
+                if (3 == marker.getSnippet().split("/").length) {
+                    View v = getLayoutInflater().inflate(R.layout.info_show_np,null);
+                    TextView name = v.findViewById(R.id.name_info_np);
+                    TextView number = v.findViewById(R.id.number_info_np);
+                    name.setText(marker.getTitle());
+                    number.setText(marker.getSnippet().split("/")[1]);
+                    return v;
+                } else {
+
+                    View v = getLayoutInflater().inflate(R.layout.info_show_p,null);
+                    TextView name = v.findViewById(R.id.name_info);
+                    TextView number = v.findViewById(R.id.number_info);
+                    TextView info = v.findViewById(R.id.info_info);
+                    name.setText(marker.getTitle());
+                    number.setText(marker.getSnippet().split("/")[0]);
+                    info.setText(marker.getSnippet().split("/")[1]);
+                    return v;
+                }
+
             }
         });
 
@@ -109,9 +124,11 @@ public class Driver_map_activity extends FragmentActivity implements OnMapReadyC
 
 
     private  void set_marker(String name,String number ,String address, LatLng marker_position, boolean is_people) {
+        String people = "true";
+
         if (is_people) {
-            mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.color.MyBlue))
-                    .position(marker_position).title(name).snippet(number + "/" + address));
+            mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                    .position(marker_position).title(name).snippet(number + "/" + address + "/" + people));
 
         } else {
             mMap.addMarker(new MarkerOptions().position(marker_position).title(name).snippet(number + "/" + address));
@@ -153,23 +170,44 @@ public class Driver_map_activity extends FragmentActivity implements OnMapReadyC
 
     @Override
     public void onInfoWindowClick(Marker marker) {
-        String number ="tel:" + marker.getSnippet().split("/")[1];
-        final Intent intent = new Intent(Intent.ACTION_DIAL);
-        intent.setData(Uri.parse(number));
 
-        AlertDialog alertDialog = new AlertDialog.Builder(this).setMessage(R.string.do_you_want_to_call)
-                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                startActivity(intent);
-            }
-        }).setNegativeButton(R.string.no, null).create();
-        alertDialog.show();
+            if (3 == marker.getSnippet().split("/").length) {
+            String number = "tel:" + marker.getSnippet().split("/")[1];
+            final Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse(number));
+
+            AlertDialog alertDialog = new AlertDialog.Builder(this).setMessage("Пояснення: " + marker.getSnippet().split("/")[0] + ". Зателефонувати?")
+                    .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            startActivity(intent);
+                        }
+                    }).setNegativeButton(R.string.no, null).create();
+            alertDialog.show();
+
+        } else {
+            String number = "tel:" + marker.getSnippet().split("/")[1];
+            final Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse(number));
+
+            AlertDialog alertDialog = new AlertDialog.Builder(this).setMessage(R.string.do_you_want_to_call)
+                    .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            startActivity(intent);
+                        }
+                    }).setNegativeButton(R.string.no, null).create();
+            alertDialog.show();
+
+        }
+    }
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 
     @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
+    public void onClick(View v) {
 
     }
 }
---
+++ app/src/main/java/ua/study/transporthelper/activity/Driver/Drv_info_activity.java
@@ -0,0 +1,17 @@
+package ua.study.transporthelper.activity.Driver;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ua.study.transporthelper.R;
+
+//TODO Написать обьснение волонтеру по карте
+public class Drv_info_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_info);
+    }
+}
